5617c0331b1dbda6758d7bbaea50a352f21a431b
LapseInJudgement <ahmadkawkab.official@gmail.com>
2025-01-06 12:11:37 +0200

    Entities

---
+++ Models/IdscommunityPlatformContext.cs
@@ -0,0 +1,391 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace IDSCommunityProject.Models;
+
+public partial class IdscommunityPlatformContext : DbContext
+{
+    public IdscommunityPlatformContext()
+    {
+    }
+
+    public IdscommunityPlatformContext(DbContextOptions<IdscommunityPlatformContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<Comment> Comments { get; set; }
+
+    public virtual DbSet<Follow> Follows { get; set; }
+
+    public virtual DbSet<Media> Media { get; set; }
+
+    public virtual DbSet<Moderation> Moderations { get; set; }
+
+    public virtual DbSet<Notification> Notifications { get; set; }
+
+    public virtual DbSet<Permission> Permissions { get; set; }
+
+    public virtual DbSet<Post> Posts { get; set; }
+
+    public virtual DbSet<Reaction> Reactions { get; set; }
+
+    public virtual DbSet<Report> Reports { get; set; }
+
+    public virtual DbSet<Role> Roles { get; set; }
+
+    public virtual DbSet<RolePermission> RolePermissions { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    public virtual DbSet<UserCategory> UserCategories { get; set; }
+
+    public virtual DbSet<UserRole> UserRoles { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=DESKTOP-1S9G528\\SQLEXPRESS;Database=IDSCommunityPLatform;Trusted_Connection=True;TrustServerCertificate=True");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Category__3214EC27DF43C480");
+
+            entity.ToTable("Category");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("ID");
+            entity.Property(e => e.Description).HasMaxLength(255);
+            entity.Property(e => e.Name)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Comment>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Comment__3214EC27F6935449");
+
+            entity.ToTable("Comment");
+
+            entity.HasIndex(e => e.CreatedAt, "CreatedAt");
+
+            entity.HasIndex(e => e.PostId, "PostID");
+
+            entity.HasIndex(e => e.UserId, "UserID");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("ID");
+            entity.Property(e => e.Content).HasMaxLength(100);
+            entity.Property(e => e.CreatedAt).HasDefaultValueSql("(getdate())");
+            entity.Property(e => e.PostId).HasColumnName("PostID");
+            entity.Property(e => e.UserId).HasColumnName("UserID");
+
+            entity.HasOne(d => d.Post).WithMany(p => p.Comments)
+                .HasForeignKey(d => d.PostId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Post_Comment");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Comments)
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("FK_User_Comment");
+        });
+
+        modelBuilder.Entity<Follow>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Follow__3214EC2762B33C81");
+
+            entity.ToTable("Follow");
+
+            entity.HasIndex(e => new { e.FollowerId, e.FolloweeId }, "FollwID");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("ID");
+            entity.Property(e => e.CreatedAt).HasDefaultValueSql("(getdate())");
+            entity.Property(e => e.FolloweeId).HasColumnName("FolloweeID");
+            entity.Property(e => e.FollowerId).HasColumnName("FollowerID");
+        });
+
+        modelBuilder.Entity<Media>(entity =>
+        {
+            entity.HasKey(e => e.MediaId).HasName("PK__Media__B2C2B5AFB9FA6661");
+
+            entity.HasIndex(e => e.RelatedId, "RelatedID");
+
+            entity.Property(e => e.MediaId).HasColumnName("MediaID");
+            entity.Property(e => e.CreatedAt).HasDefaultValueSql("(getdate())");
+            entity.Property(e => e.Description).HasColumnType("text");
+            entity.Property(e => e.MediaType)
+                .HasMaxLength(10)
+                .IsUnicode(false);
+            entity.Property(e => e.MediaUrl)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.RelatedId).HasColumnName("RelatedID");
+            entity.Property(e => e.RelatedType)
+                .HasMaxLength(10)
+                .IsUnicode(false);
+            entity.Property(e => e.UpdatedAt).HasDefaultValueSql("(getdate())");
+        });
+
+        modelBuilder.Entity<Moderation>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Moderati__3214EC27E89F65DC");
+
+            entity.ToTable("Moderation");
+
+            entity.HasIndex(e => new { e.RoleId, e.RelatedId }, "MdID");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("ID");
+            entity.Property(e => e.CreatedAt).HasDefaultValueSql("(getdate())");
+            entity.Property(e => e.Reason).HasMaxLength(255);
+            entity.Property(e => e.RelatedId).HasColumnName("RelatedID");
+            entity.Property(e => e.RoleId).HasColumnName("RoleID");
+
+            entity.HasOne(d => d.Role).WithMany(p => p.Moderations)
+                .HasForeignKey(d => d.RoleId)
+                .HasConstraintName("FK_Role_Mod");
+        });
+
+        modelBuilder.Entity<Notification>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Notifica__3214EC27FF0EE10D");
+
+            entity.ToTable("Notification");
+
+            entity.HasIndex(e => e.CreatedAt, "CreatedAt");
+
+            entity.HasIndex(e => e.CreatedAt, "ReactionID");
+
+            entity.HasIndex(e => e.CreatedAt, "RelatedID");
+
+            entity.HasIndex(e => e.UserId, "UserID");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("ID");
+            entity.Property(e => e.ActionType).HasMaxLength(50);
+            entity.Property(e => e.CreatedAt).HasDefaultValueSql("(getdate())");
+            entity.Property(e => e.Message).HasMaxLength(50);
+            entity.Property(e => e.ReactionId).HasColumnName("ReactionID");
+            entity.Property(e => e.RelatedId).HasColumnName("RelatedID");
+            entity.Property(e => e.RelatedType)
+                .HasMaxLength(10)
+                .IsUnicode(false);
+            entity.Property(e => e.UserId).HasColumnName("UserID");
+
+            entity.HasOne(d => d.Reaction).WithMany(p => p.Notifications)
+                .HasForeignKey(d => d.ReactionId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Reaction_Notf");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Notifications)
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("FK_User_Notf");
+        });
+
+        modelBuilder.Entity<Permission>(entity =>
+        {
+            entity.HasKey(e => e.PermissionId).HasName("PK__Permissi__EFA6FB0F831AAA54");
+
+            entity.ToTable("Permission");
+
+            entity.HasIndex(e => e.ActionName, "UQ__Permissi__491F5DD0FD6E9C89").IsUnique();
+
+            entity.Property(e => e.PermissionId).HasColumnName("PermissionID");
+            entity.Property(e => e.ActionName).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Post>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Post__3214EC270C43D3B2");
+
+            entity.ToTable("Post");
+
+            entity.HasIndex(e => e.CreatedAt, "CreatedAt");
+
+            entity.HasIndex(e => e.UserId, "UserID");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("ID");
+            entity.Property(e => e.Content).HasMaxLength(100);
+            entity.Property(e => e.CreatedAt).HasDefaultValueSql("(getdate())");
+            entity.Property(e => e.Title).HasMaxLength(100);
+            entity.Property(e => e.UserId).HasColumnName("UserID");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Posts)
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("FK_User");
+        });
+
+        modelBuilder.Entity<Reaction>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Reaction__3214EC271FC38109");
+
+            entity.ToTable("Reaction");
+
+            entity.HasIndex(e => e.CreatedAt, "RelatedID");
+
+            entity.HasIndex(e => e.UserId, "UserID");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("ID");
+            entity.Property(e => e.CreatedAt).HasDefaultValueSql("(getdate())");
+            entity.Property(e => e.ReactionType).HasMaxLength(10);
+            entity.Property(e => e.RelatedId).HasColumnName("RelatedID");
+            entity.Property(e => e.RelatedType)
+                .HasMaxLength(10)
+                .IsUnicode(false);
+            entity.Property(e => e.UserId).HasColumnName("UserID");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Reactions)
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("FK_User_Reaction");
+        });
+
+        modelBuilder.Entity<Report>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Report__3214EC272687F329");
+
+            entity.ToTable("Report");
+
+            entity.HasIndex(e => new { e.UserId, e.PostId, e.CommentId }, "RepID");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("ID");
+            entity.Property(e => e.CommentId).HasColumnName("CommentID");
+            entity.Property(e => e.PostId).HasColumnName("PostID");
+            entity.Property(e => e.Reason).HasMaxLength(255);
+            entity.Property(e => e.ReportedType)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+            entity.Property(e => e.Status).HasMaxLength(50);
+            entity.Property(e => e.UserId).HasColumnName("UserID");
+
+            entity.HasOne(d => d.Comment).WithMany(p => p.Reports)
+                .HasForeignKey(d => d.CommentId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Comment_Report");
+
+            entity.HasOne(d => d.Post).WithMany(p => p.Reports)
+                .HasForeignKey(d => d.PostId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_Post_Report");
+
+            entity.HasOne(d => d.User).WithMany(p => p.Reports)
+                .HasForeignKey(d => d.UserId)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_User_Report");
+        });
+
+        modelBuilder.Entity<Role>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__Role__8AFACE3A266D50DE");
+
+            entity.ToTable("Role");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.RoleName).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<RolePermission>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__RolePerm__3214EC27E5B4F93D");
+
+            entity.ToTable("RolePermission");
+
+            entity.HasIndex(e => new { e.RoleId, e.PermissionId }, "RolePID");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.PermissionId).HasColumnName("PermissionID");
+            entity.Property(e => e.RoleId).HasColumnName("RoleID");
+
+            entity.HasOne(d => d.Permission).WithMany(p => p.RolePermissions)
+                .HasForeignKey(d => d.PermissionId)
+                .HasConstraintName("FK_Permission");
+
+            entity.HasOne(d => d.Role).WithMany(p => p.RolePermissions)
+                .HasForeignKey(d => d.RoleId)
+                .HasConstraintName("FK_Role");
+        });
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__USER__3214EC276C474E1B");
+
+            entity.ToTable("USER");
+
+            entity.HasIndex(e => e.Email, "Email").IsUnique();
+
+            entity.HasIndex(e => e.Username, "UserName").IsUnique();
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.Age).HasColumnName("age");
+            entity.Property(e => e.Email)
+                .HasMaxLength(100)
+                .HasColumnName("email");
+            entity.Property(e => e.Country)
+                .HasMaxLength(100)
+                .HasColumnName("country");
+            entity.Property(e => e.PasswordHash).HasMaxLength(255);
+            entity.Property(e => e.RoleId).HasColumnName("RoleID");
+            entity.Property(e => e.Username)
+                .HasMaxLength(50)
+                .HasColumnName("username");
+        });
+
+        modelBuilder.Entity<UserCategory>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__UserCate__3214EC2775323F03");
+
+            entity.ToTable("UserCategory");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("ID");
+            entity.Property(e => e.About)
+                .HasMaxLength(255)
+                .IsUnicode(false);
+            entity.Property(e => e.Title)
+                .HasMaxLength(20)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<UserRole>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__UserRole__3214EC27112C9BEC");
+
+            entity.ToTable("UserRole");
+
+            entity.HasIndex(e => new { e.RoleId, e.UserId }, "UserRID");
+
+            entity.Property(e => e.Id).HasColumnName("ID");
+            entity.Property(e => e.RoleId).HasColumnName("RoleID");
+            entity.Property(e => e.UserId).HasColumnName("UserID");
+
+            entity.HasOne(d => d.Role).WithMany(p => p.UserRoles)
+                .HasForeignKey(d => d.RoleId)
+                .HasConstraintName("FK_Role_Permission");
+
+            entity.HasOne(d => d.User).WithMany(p => p.UserRoles)
+                .HasForeignKey(d => d.UserId)
+                .HasConstraintName("FK_User_Role");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Models/Reaction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace IDSCommunityProject.Models;
+
+public partial class Reaction
+{
+    public int Id { get; set; }
+
+    public int UserId { get; set; }
+
+    public string RelatedType { get; set; } = null!;
+
+    public int RelatedId { get; set; }
+
+    public string ReactionType { get; set; } = null!;
+
+    public DateTime CreatedAt { get; set; }
+
+    public virtual ICollection<Notification> Notifications { get; set; } = new List<Notification>();
+
+    public virtual User User { get; set; } = null!;
+}
